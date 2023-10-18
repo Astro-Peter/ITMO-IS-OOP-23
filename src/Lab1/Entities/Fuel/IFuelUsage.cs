1c989a12525ed49ccd78066a072d7cef26b69a65
Astro-Peter <Vanst63@yandex.ru>
2023-10-18 18:44:09 +0300

    refactor: deleted unused code

--- src/Lab1/Entities/Fuel/IFuelUsage.cs
+++
@@ -1,9 +0,0 @@
-﻿using Itmo.ObjectOrientedProgramming.Lab1.Entities.FuelMarket;
-
-namespace Itmo.ObjectOrientedProgramming.Lab1.Entities.Fuel;
-
-public interface IFuelUsage
-{
-    public double GetFuelCost(IFuelMarket market);
-    public void AddFuelUsage(int id, double fuelUsed);
-}
