250b715112c0e9873204f22deec9bce41aaf5eed
Astro-Peter <Vanst63@yandex.ru>
2023-11-08 23:28:27 +0300

    feat: add file logger and file display driver

---
+++ src/Lab3/Entities/DisplayDriver/FileDisplayDriver.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Entities.DisplayDriver;
+
+public class FileDisplayDriver : IDisplayDriver
+{
+    private readonly StreamWriter _displayFile;
+    private Color _color;
+
+    public FileDisplayDriver(Color color, string fileName)
+    {
+        _color = color;
+        _displayFile = File.AppendText(fileName);
+    }
+
+    public void Clear()
+    {
+        _displayFile.Flush();
+    }
+
+    public void SetColor(Color color)
+    {
+        _color = color;
+    }
+
+    public void Write(string message)
+    {
+        _displayFile.Write(Crayon.Output.Rgb(_color.R, _color.G, _color.B).Text(message));
+    }
+}
---
+++ src/Lab3/Entities/Loggers/FileLogger.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Itmo.ObjectOrientedProgramming.Lab3.Models;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Entities.Loggers;
+
+public class FileLogger : ILogger
+{
+    private readonly StreamWriter _logFile;
+
+    public FileLogger(string fileName)
+    {
+        _logFile = File.AppendText(fileName);
+    }
+
+    private int Cnt { get; set; }
+
+    public void LogMessage(Message message)
+    {
+        _logFile.WriteLine("Message #{0}", ++Cnt);
+        _logFile.WriteLine(message.Header);
+        _logFile.WriteLine(message.PriorityLevel);
+    }
+}
