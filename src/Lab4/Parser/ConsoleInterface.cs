e3797858dd5b7b8d758642ee8cefbec709d0ab61
Astro-Peter <Vanst63@yandex.ru>
2023-12-01 16:29:15 +0300

    feat: make console interface

---
+++ src/Lab4/Parser/ConsoleInterface.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Itmo.ObjectOrientedProgramming.Lab4.Entities.Context;
+using Itmo.ObjectOrientedProgramming.Lab4.Model;
+using Itmo.ObjectOrientedProgramming.Lab4.Parser.Chain;
+using Itmo.ObjectOrientedProgramming.Lab4.Parser.Chain.ChainLink;
+using Itmo.ObjectOrientedProgramming.Lab4.Parser.Parser;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4.Parser;
+
+public static class ConsoleInterface
+{
+    public static void Start()
+    {
+        ILink start = RegularChainBuilder.Build();
+        IParser parser = new Parser.Parser(start);
+        IContext context = new Context();
+        while (Console.ReadLine() is { } request && request != ":q")
+        {
+            ParsingResult builder = parser.Parse(request);
+            if (builder is ParsingResult.Success successParsingResult)
+            {
+                BuildResult command = successParsingResult.Builder.Build();
+                if (command is BuildResult.BuildSuccess s)
+                {
+                    CommandResult commandResult = s.Command.Execute(context);
+                    if (commandResult is CommandResult.Failure failure)
+                    {
+                        Console.WriteLine(failure.Result);
+                    }
+                }
+                else if (command is BuildResult.BuildFailure bf)
+                {
+                    Console.WriteLine(bf.Message);
+                }
+            }
+            else if (builder is ParsingResult.Failure f)
+            {
+                Console.WriteLine(f.Issue);
+            }
+        }
+    }
+}
