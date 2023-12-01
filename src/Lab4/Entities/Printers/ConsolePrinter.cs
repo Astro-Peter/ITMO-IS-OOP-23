a5c4af5c00a2d72504845de9e47d84da9b280560
Astro-Peter <Vanst63@yandex.ru>
2023-12-01 16:28:18 +0300

    feat: make console printer

---
+++ src/Lab4/Entities/Printers/ConsolePrinter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4.Entities.Printers;
+
+public class ConsolePrinter : IPrinter
+{
+    public void Print(string data)
+    {
+        Console.WriteLine(data);
+    }
+}
