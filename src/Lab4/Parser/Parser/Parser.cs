5dfe5d00a9ec374549ba3f73ac0a6636cffc050d
Astro-Peter <Vanst63@yandex.ru>
2023-12-01 16:28:37 +0300

    feat: make parser

---
+++ src/Lab4/Parser/Parser/Parser.cs
@@ -0,0 +1,21 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab4.Entities.RequestIterator;
+using Itmo.ObjectOrientedProgramming.Lab4.Model;
+using Itmo.ObjectOrientedProgramming.Lab4.Parser.Chain.ChainLink;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4.Parser.Parser;
+
+public class Parser : IParser
+{
+    private readonly ILink _linkStart;
+
+    public Parser(ILink linkStart)
+    {
+        _linkStart = linkStart;
+    }
+
+    public ParsingResult Parse(string request)
+    {
+        var splitRequest = new RequestIterator(request);
+        return _linkStart.Handle(splitRequest);
+    }
+}
